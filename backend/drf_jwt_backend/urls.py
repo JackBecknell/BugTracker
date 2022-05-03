b883889422fc9d383107008eb5031434196ced99
JackBecknell <52moosefit@gmail.com>
2022-05-03 14:26:07 -0400

    Deleted demo app 'cars' + created new App 'tickets' + created new App 'projects'

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path('api/cars/', include('cars.urls')),
 ]
--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Car
 
 # Register your models here.
-admin.site.register(Car)
