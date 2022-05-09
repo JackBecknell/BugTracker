11e5902081ffc5e35e7b029ae5efae7dd4f24161
JackBecknell <52moosefit@gmail.com>
2022-05-09 13:50:05 -0400

    In frontend InspectTicket.jsx updated comments, deleted useEffect on line.30, declared variable reloadConditions that holds id(from useParams) and requestReload(bool useState) then set the useEffect on line.37 to moniter it, in form component line.118 I added onKeyPress which looks for user to press the enter key which will act as a submit for the comment and post it, in if statment line.140 I sliced and reverse mapped the comments returned by axios so that the newest comments would be at the top, In the return statment line 208 I deleted the conditional logic for rendering post form and added it into the conditional logic for comments so that now one button will collapse both the post comment form and all comments. + Updated InspectTicket.css styles. + Updated DashBoardStyles.css styels. + In DashBoard.js I updated imports, added useState boolean requestReload and set state to true, I declared a variable reloadConditions set equal to both token(from useAuth import) and to requestReload, In useEffect I changed the monitered variable from token to reloadConditions, In return statment I added imported component AddProject and passed setRequestReload into it and then wrapped it and projects-head in a div. + In DeleteTicketStyles.css I updated styles. + In AddTicketStyles.css I updated styles. + In AddProjectStyles.css I updated styles. + In ProjectModal.jsx I added imports, declared function AddProjectModal and export default AddProjectModal, Inside the function I added const navigate set equal to useNavigate, added [user, token] from useAuth(), declared useState variable projectTitle and useState variable projectDescription, added postProject as async function call for post functionality, Added handlSubmit function (builds the new project object and passes it to axios call) to be called when form in return statment is submitted, added return statment which displays full screen modal with cancel button for closing it and a submit button for posting it with two text boxes(one for project title one for project description). + In AddProject.jsx I added imports, declared AddProject and set it to default export, declared a useState boolean variable modalStatus, added return statment which conditionaly renders imported component AddProjectModal if user has clicked the 'add' button.

---
+++ frontend/src/components/AddProject/AddProjectModal.jsx
@@ -0,0 +1,81 @@
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import "./AddProjectStyles.css";
+
+function AddProjectModal(props) {
+  const navigate = useNavigate();
+  const [user, token] = useAuth();
+  //Single values
+  const [projectTitle, setProjectTitle] = useState("");
+  const [projectDescrip, setProjectDescrip] = useState("");
+
+  const postProject = async (newProject) => {
+    try {
+      await axios.post("http://127.0.0.1:8000/api/projects/post/", newProject, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      props.reloadProject(true);
+      props.setModalStatus(false);
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //function creates project for axios
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newProject = {
+      title: projectTitle,
+      description: projectDescrip,
+      project_author_id: user.id,
+    };
+    postProject(newProject);
+  }
+
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <form onSubmit={handleSubmit}>
+          <div className="cancel-submit-btns">
+            <button
+              onClick={() => props.setModalStatus(false)}
+              className="cancel-Btn"
+            >
+              CANCEL
+            </button>
+            <button type="submit" className="ticket-Submit-Btn">
+              SUBMIT
+            </button>
+          </div>
+          <div className="create-new-project">
+            <h4>CREATE NEW PROJECT</h4>
+          </div>
+          <h4>TITLE</h4>
+          <input
+            rows="1"
+            className="auto_height"
+            maxlength="40"
+            type="text"
+            value={projectTitle}
+            onChange={(event) => setProjectTitle(event.target.value)}
+          />
+          <h4>DESCRIPTION</h4>
+          <textarea
+            rows="4"
+            className="auto_height"
+            maxlength="1250"
+            type="text"
+            value={projectDescrip}
+            onChange={(event) => setProjectDescrip(event.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+export default AddProjectModal;
