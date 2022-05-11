4db03e339b4211cc1843b21895aa06e691819cd6
JackBecknell <52moosefit@gmail.com>
2022-05-11 13:58:35 -0400

    Updated information regarding admin.jsx

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
-    # path('all/', )
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -3,9 +3,6 @@ from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.response import Response
-from rest_framework.decorators import APIView
-from rest_framework.permissions import IsAuthenticated
 User = get_user_model()
 
 
@@ -18,5 +15,4 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
 
-class GetAllUsers(APIView):
-    users = User.objects.all()
+
